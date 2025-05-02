0e0fbbe0c0546adcdcb5dcb449f548ae091b6ff9
yuyuu-8 <sishichulu@gmail.com>
2025-05-02 10:41:20 +0300

    feat: add admin menu

--- includes/admin_menu.h
+++ includes/admin_menu.h
@@ -3,8 +3,18 @@
 
 #include <sqlite3.h>
 
+void showAdminMenu(sqlite3* db);
+
+void addNewDriver(sqlite3* db);
+
 void addNewCar(sqlite3* db);
+
 void addNewOrder(sqlite3* db);
 
-#endif
+void updateDriver(sqlite3* db);
 
+void deleteDriver(sqlite3* db);
+
+void showAdminStats(sqlite3* db);
+
+#endif
